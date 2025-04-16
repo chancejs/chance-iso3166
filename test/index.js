58972292835925a0f2fab3e343bb12e36d30e564
leesei <leesei@gmail.com>
2025-04-16 13:24:43 +0800

    update deps and test

--- test/index.js
+++ test/index.js
@@ -1,38 +1,39 @@
-const test = require('tape');
-const chance = require('chance').Chance('seed');
+const test = require("tape");
+// instance with deterministic seed
+const chance = require("chance").Chance("seed");
 
 chance.mixin({
-  iso3166: require('..')
+  iso3166: require(".."),
 });
 
-test('generate country 1', function (t) {
+test("generate country 1", function (t) {
   t.deepEqual(chance.iso3166(), {
-    name: 'Georgia',
-    alpha2: 'GE',
-    alpha3: 'GEO',
-    numeric: '268'
+    name: "Gambia",
+    alpha2: "GM",
+    alpha3: "GMB",
+    numeric: "270",
   });
 
   t.end();
 });
 
-test('generate country 2', function (t) {
+test("generate country 2", function (t) {
   t.deepEqual(chance.iso3166(), {
-    name: 'Suriname',
-    alpha2: 'SR',
-    alpha3: 'SUR',
-    numeric: '740'
+    name: "Sudan",
+    alpha2: "SD",
+    alpha3: "SDN",
+    numeric: "729",
   });
 
   t.end();
 });
 
-test('generate country 3', function (t) {
+test("generate country 3", function (t) {
   t.deepEqual(chance.iso3166(), {
-    name: 'Turkey',
-    alpha2: 'TR',
-    alpha3: 'TUR',
-    numeric: '792'
+    name: "Türkiye",
+    alpha2: "TR",
+    alpha3: "TUR",
+    numeric: "792",
   });
 
   t.end();
